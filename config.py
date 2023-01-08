2c2092957576116c5d4b86c09d36efacdee9d7b5
Hiyabo <99443606+hiyabo69@users.noreply.github.com>
2023-01-08 12:25:55 -0500

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ TG_ADMIN = 'Hiyabo'
 DB_LOCAL = False
 DB_HOST = 'sql.freedb.tech'
 DB_HOST_USERNAME = 'freedb_josemanuel'
-DB_HOST_PASSWORD = '@9d6p7shnZU#N3h'
+DB_HOST_PASSWORD = 'TkM$b&?Jp28W7@e'
 DB_NAME = 'freedb_educaDb'
 
 if DB_LOCAL:
